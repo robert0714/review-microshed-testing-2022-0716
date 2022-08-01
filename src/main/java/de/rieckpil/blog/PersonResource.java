6f3ff410c274f8427e9b007888a4fc3aba11c55a
robert0714 <robert0714@gmail.com>
2022-08-01 09:30:55 +0800

    init commit

---
+++ src/main/java/de/rieckpil/blog/PersonResource.java
@@ -0,0 +1,63 @@
+package de.rieckpil.blog;
+
+import java.net.URI;
+import java.util.List;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+import javax.transaction.Transactional.TxType;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.NotFoundException;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
+import javax.ws.rs.core.UriInfo;
+
+import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
+
+@Path("/persons")
+@ApplicationScoped
+@Transactional(TxType.REQUIRED)
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+public class PersonResource {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @GET
+    public List<Person> getAllPersons() {
+        return entityManager.createQuery("SELECT p FROM Person p", Person.class).getResultList();
+    }
+
+    @GET
+    @Path("/{id}")
+    public Person getPersonById(@PathParam("id") Long id) {
+    	Person personById = entityManager.find(Person.class, id);
+
+        if (personById == null) {
+            throw new NotFoundException();
+        }
+
+        return personById;
+    }
+
+    @POST
+    public Response createNewPerson(@Context UriInfo uriInfo, @RequestBody Person personToStore) {
+        entityManager.persist(personToStore);
+
+        URI headerLocation = uriInfo.getAbsolutePathBuilder()
+                .path(personToStore.getId().toString())
+                .build();
+
+        return Response.created(headerLocation).build();
+    }
+}
